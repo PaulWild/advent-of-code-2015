19f2ccd85579c0b8fde1859722312c4affea1225
Paul Wild <paulwild@gmail.com>
2020-11-28 19:31:27 +0000

    Day 4

---
+++ AdventOfCode.Tests/Days/Day04Tests.cs
@@ -0,0 +1,36 @@
+using System;
+using AdventOfCode.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Tests.Days
+{
+    
+    public class Day4Tests
+    {
+        private readonly ISolution _sut = new Day4();
+        
+        [Fact]
+        public void PartOne_WhenCalled_DoesNotThrowNotImplementedException()
+        {
+            Action act = () =>  _sut.PartOne(_sut.Input());
+            
+            act.Should().NotThrow<NotImplementedException>();
+        }
+        
+        [Fact]
+        public void PartTwo_WhenCalled_DoesNotThrowNotImplementedException()
+        {
+            Action act = () =>  _sut.PartTwo(_sut.Input());
+            
+            act.Should().NotThrow<NotImplementedException>();
+        }
+        
+        [Fact]
+        public void PartOne_WhenCalled_CalculatesHash()
+        {
+            var result =  _sut.PartOne(new[] {"abcdef"});
+            result.Should().Be("609043");
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day4.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace AdventOfCode.Days
+{
+    public class Day4 : ISolution
+    {
+        public string PartOne(string[] input)
+        {
+            return Process(input, "00000");
+        }
+        
+        public string PartTwo(string[] input)
+        {
+            return Process(input, "000000");
+        }
+        
+        private static string Process(string[] input, string startsWith)
+        {
+            var parsed = input.First();
+
+            int value = 0;
+            while (true)
+            {
+                var toHash = parsed + value;
+                var blah = MD5.HashData(System.Text.Encoding.ASCII.GetBytes(toHash));
+
+                var result = Convert.ToHexString(blah);
+                if (result.StartsWith(startsWith))
+                    return value.ToString();
+
+                value++;
+            }
+        }
+
+
+        public int Day => 4;
+    }
+}
