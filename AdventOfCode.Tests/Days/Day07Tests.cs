f317acc5ac3c857fae9da1ce0cf75021a7f379c1
Paul Wild <paulwild@gmail.com>
2020-11-29 01:09:48 +0000

    Day 7

---
+++ AdventOfCode.Tests/Days/Day07Tests.cs
@@ -0,0 +1,40 @@
+using System;
+using AdventOfCode.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Tests.Days
+{
+
+    public class Day7Tests
+    {
+        private readonly ISolution _sut = new Day7();
+
+        [Fact]
+        public void PartOne_WhenCalled_DoesNotThrowNotImplementedException()
+        {
+            Action act = () => _sut.PartOne(_sut.Input());
+
+            act.Should().NotThrow<NotImplementedException>();
+        }
+
+        [Fact]
+        public void PartOne_WhenCalled_Returns()
+        {
+            var input = new[]
+            {
+                "123 -> x",
+                "456 -> y",
+                "x AND y -> d",
+                "x OR y -> e",
+                "x LSHIFT 2 -> f",
+                "y RSHIFT 2 -> g",
+                "NOT x -> h",
+                "NOT y -> i"
+            };
+
+            var result = _sut.PartOne(input);
+            result.Should().Be("d = 72,e = 507,f = 492,g = 114,h = 65412,i = 65079,x = 123,y = 456");
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day7.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Days
+{
+    public class Day7 : ISolution
+    {
+        public string PartOne(string[] input)
+        {
+            var parsed = ParseInput(input).ToList();
+            var cache = new ConcurrentDictionary<string, int>();
+
+            var foo = new List<(string wire, int signal)>();
+            foreach (var (wire,_) in parsed)
+            {
+                var res = GetValue(wire, parsed, cache);
+                foo.Add((wire, res));
+            }
+
+            return string.Join(",", foo.OrderBy(x => x.wire).Select(x => $"{x.wire} = {(ushort)x.signal}").ToList());
+
+        }
+
+        public int GetValue(string wire, List<(string wire, Operation operation)> operations, ConcurrentDictionary<string, int> cache)
+        {
+            var (_, operation) = operations.Single(x => x.wire == wire);
+
+            return cache.GetOrAdd(wire, x =>
+
+                operation.Command switch
+                {
+                    "VALUE" => int.TryParse(operation.L, out _)
+                        ? Convert.ToInt32(operation.L)
+                        : GetValue(operation.L, operations, cache),
+                    "AND" => (int.TryParse(operation.L, out _)
+                        ? Convert.ToInt32(operation.L)
+                        : GetValue(operation.L, operations, cache)) & GetValue(operation.R, operations, cache),
+                    "OR" => GetValue(operation.L, operations, cache) | GetValue(operation.R, operations, cache),
+                    "LSHIFT" => GetValue(operation.L, operations, cache) << Convert.ToInt32(operation.R),
+                    "RSHIFT" => GetValue(operation.L, operations, cache) >> Convert.ToInt32(operation.R),
+                    "NOT" => ~GetValue(operation.R, operations, cache),
+                    _ => throw new ArgumentOutOfRangeException()
+
+                });
+        }
+        
+        public string PartTwo(string[] input)
+        {
+            var parsed = ParseInput(input).ToList();
+            var cache = new ConcurrentDictionary<string, int>();
+            cache.GetOrAdd("b", 3176);
+
+            var foo = new List<(string wire, int signal)>();
+            foreach (var (wire,_) in parsed)
+            {
+                var res = GetValue(wire, parsed, cache);
+                foo.Add((wire, res));
+            }
+
+            return string.Join(",", foo.OrderBy(x => x.wire).Select(x => $"{x.wire} = {(ushort)x.signal}").ToList());
+        }
+
+        public IEnumerable<(string wire, Operation operation)> ParseInput(string[] input)
+        {
+            foreach (var str in input)
+            {
+                var operation = Regex.Match(str, "(?'left'[0-9a-z]+) (?'op'OR|AND|RSHIFT|LSHIFT) (?'right'[0-9a-z]+) -> (?'wire'[a-z]+)");
+                var not = Regex.Match(str, "NOT (?'right'[a-z]+) -> (?'wire'[a-z]+)");
+                var value = Regex.Match(str, "(?'left'[0-9a-z]+) -> (?'wire'[a-z]+)");
+
+                if (operation.Success)
+                {
+                    yield return (operation.Groups["wire"].Value, new Operation()
+                    {
+                        Command = operation.Groups["op"].Value,
+                        L = operation.Groups["left"].Value,
+                        R = operation.Groups["right"].Value
+                    });
+                }
+                else if (not.Success)
+                {
+                    yield return (not.Groups["wire"].Value ,new Operation()
+                    {
+                        Command = "NOT",
+                        R = not.Groups["right"].Value
+                    });
+                }
+                else if (value.Success)
+                {
+                    yield return (value.Groups["wire"].Value ,new Operation()
+                    {
+                        Command = "VALUE",
+                        L = value.Groups["left"].Value
+                    });
+                }
+                else
+                {
+                    throw new Exception($"Fuck you: {str}");
+                }
+                
+            }
+        }
+
+        public int Day => 7;
+        public record Operation
+        {
+            public string Command { get; init; }
+            public string L { get; init; }
+            public string R { get; init; }
+            
+        }
+    }
+}
